afc88fbe0c8a590f05fdedbc99fae91f22d2d249
atticusofsparta <atticusofsparta@protonmail.com>
2025-03-26 23:19:02 -0600

    chore(init): initial project setup

---
+++ src/main.tsx
@@ -0,0 +1,53 @@
+import {
+  RouterProvider,
+  createHashHistory,
+  createRouter,
+} from '@tanstack/react-router';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Import the generated route tree
+import '@fontsource/cormorant-infant';
+import { routeTree } from './routeTree.gen';
+
+const queryClient = new QueryClient();
+
+const hashHistory = createHashHistory();
+// Create a new router instance
+
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  history: hashHistory,
+  context: {
+    queryClient,
+  },
+  defaultPreload: 'intent',
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
+  scrollRestoration: true,
+});
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>,
+  );
+}
